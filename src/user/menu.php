033a74a004d74c7169e30a81ffdf3beceb316995
Pranpriya <s6506021622027@email.kmutnb.ac.th>
2024-03-10 03:47:43 +0700

    4r

---
+++ src/user/menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./src/css/style.css">
+</head>
+<body>
+<ul>
+    <li>Menu1</li>
+    <li>Menu2</li>
+    <li>Menu3</li>
+</ul>
+</body>
+</html>
