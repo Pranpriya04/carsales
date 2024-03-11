dd5ff9e798fb0f6c63d635c839b73a044e70b1d2
Pranpriya <s6506021622027@email.kmutnb.ac.th>
2024-03-11 16:33:50 +0700

    wfw

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>  
+    
+<div class="header">
+<div class="logo">
+            <h1 id="logo">FDPG CARS</h1>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-house"></i>Login</a></li>
+                <li><a href="#"><i class="fa-solid fa-envelope"></i>Contact</a></li>
+            </ul>
+        </div>
+</div>
+<div class="slider">
+    <img src="./pictures/4.jpg" alt="Image 1" >
+    <img src="./pictures/2.jpg" alt="Image 2" >
+    <img src="./pictures/3.jpg" alt="Image 3" >
+    <div class="con">If "GOOD CAR" is your choice so It's actully "US"</div>
+</div>
+
+
+
+</body>
+</html>
