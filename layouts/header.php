b4070c3def5528734bdffa1d436353814d9c1b3f
Tanongsak Intean <71477852+tanongsakintean@users.noreply.github.com>
2024-03-13 08:08:31 +0700

    afs

--- layouts/header.php
+++ layouts/header.php
@@ -4,24 +4,7 @@
             <h1 id="logo">FDPG CARS</h1>
         </a>
     </div>
-    <div class="dp">
-        <div class="dropdown">
-            <h6 class="dropbtn">ประเภทรถ</h6>
-            <div class="dropdown-content">
-                <a href="#">Option 1</a>
-                <a href="#">Option 2</a>
-                <a href="#">Option 3</a>
-            </div>
-        </div>
-        <div class="dropdown">
-            <h6 class="dropbtn">แบรนด์รถ</h6>
-            <div class="dropdown-content">
-                <a href="#">Option 1</a>
-                <a href="#">Option 2</a>
-                <a href="#">Option 3</a>
-            </div>
-        </div>
-    </div>
+
     <div class="menu">
         <ul>
             <?php
@@ -32,7 +15,6 @@
             <?php } else { ?>
                 <li><a href="login.php"><i class="fa-solid fa-house"></i>Login</a></li>
             <?php } ?>
-            <li><a href="#"><i class="fa-solid fa-envelope"></i>Contact</a></li>
         </ul>
     </div>
 </div>
